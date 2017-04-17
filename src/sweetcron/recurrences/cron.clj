a28aba5c0bfa44debbdb043b15544a6c04982854
James Aley <james.aley@gmail.com>
2017-04-17 12:17:14 +0100

    Tis broked

--- src/sweetcron/recurrences/cron.clj
+++ src/sweetcron/recurrences/cron.clj
@@ -11,14 +11,16 @@
   "Rolls a date foward according to time unit rules specified in other
   params:
 
+    t-next     - the time instant being proposed for next occurrence
+    t-now      - the current time instant, for calculating next occurrence
     value      - numeric value of time unit
-    get-fn     - fn to return current value of time unit from dt
     set-fn     - fn to update dt with explicit new value of time unit
     carry-unit - on overflow, increment dt by 1 of this unit function"
-  [dt value get-fn set-fn carry-unit]
-  (let [proposed (-> dt (set-fn value) (t/floor t/minute))]
-    (cond-> dt
-      (not (t/after? proposed dt)) (t/plus (-> 1 carry-unit))
+  [t-next t-now value set-fn carry-unit]
+  (let [t-proposed (-> t-next (set-fn value) (t/floor t/minute))]
+    (cond-> t-next
+      ;; if the proposed change puts us in the past, we need to carry
+      (t/before? t-proposed t-now) (t/plus (-> 1 carry-unit))
       true                         (set-fn value))))
 
 (defn zero-components
@@ -33,20 +35,20 @@
 (defn advance-month
   "Update the month component of given date time, if the month
   parameter is a static value."
-  [dt month]
+  [t-next t-now month]
   (if (= month :*)
-    dt
-    (-> dt
-        (roll-forward month t/month (memfn withMonthOfYear m) t/years)
+    t-next
+    (-> t-next
+        (roll-forward t-now month (memfn withMonthOfYear m) t/years)
         (zero-components :day :hour :minute))))
 
 (defn advance-day-of-month
   "Update the day component according to a day-of-month constraint"
-  [dt day-of-month]
+  [t-next t-now day-of-month]
   (if (= day-of-month :*)
-    dt
-    (-> dt
-        (roll-forward day-of-month t/day (memfn withDayOfMonth d) t/months)
+    t-next
+    (-> t-next
+        (roll-forward t-now day-of-month (memfn withDayOfMonth d) t/months)
         (zero-components :hour :minute))))
 
 (defn cron-dow->joda-dow
@@ -56,22 +58,22 @@
 
 (defn advance-day-of-week
   "Update the day component according to a day-of-week constraint"
-  [dt day-of-week]
+  [t-next t-now day-of-week]
   (if (= day-of-week :*)
-    dt
+    t-next
     (let [joda-dow (cron-dow->joda-dow day-of-week)]
-      (-> dt
-          (roll-forward joda-dow t/day-of-week (memfn withDayOfWeek d) t/weeks)
+      (-> t-next
+          (roll-forward t-now joda-dow (memfn withDayOfWeek d) t/weeks)
           (zero-components :hour :minute)))))
 
 (defn advance-day
   "Update the day component to the next most recent of either the
   day-of-month or day-of-week options, if they are static."
-  [dt day-of-month day-of-week]
+  [t-next t-now day-of-month day-of-week]
   (if (= :* day-of-week day-of-month)
-    dt ;; no day constraints, runs every day
-    (let [next-dom (advance-day-of-month dt day-of-month)
-          next-dow (advance-day-of-week dt day-of-week)]
+    t-next ;; no day constraints, runs every day
+    (let [next-dom (advance-day-of-month t-next t-now day-of-month)
+          next-dow (advance-day-of-week t-next t-now day-of-week)]
       (cond
         (= :* day-of-month) next-dow
         (= :* day-of-week)  next-dom
@@ -80,28 +82,28 @@
 
 (defn advance-hour
   "Update the hour component according to the given constraint"
-  [dt hour]
+  [t-next t-now hour]
   (if (= hour :*)
-    dt
-    (-> dt
-        (roll-forward hour t/hour (memfn withHourOfDay hour) t/days)
+    t-next
+    (-> t-next
+        (roll-forward t-now hour (memfn withHourOfDay hour) t/days)
         (zero-components :minute))))
 
 (defn advance-minute
   "Update the minute component according to the given constraint."
-  [dt minute]
+  [t-next t-now minute]
   (if (= :* minute)
-    (t/plus dt (-> 1 t/minutes)) ;; always return the next minute
-    (roll-forward dt minute t/minute (memfn withMinuteOfHour minute) t/hours)))
+    (t/plus t-next (-> 1 t/minutes)) ;; always return the next minute
+    (roll-forward t-next t-now minute (memfn withMinuteOfHour minute) t/hours)))
 
 (defrecord CronRecurrence [minute hour day-of-month month day-of-week]
   r/Recurrence
   (time-of-next [r t-now]
     (-> t-now
-        (advance-month month)
-        (advance-day day-of-month day-of-week)
-        (advance-hour hour)
-        (advance-minute minute)
+        (advance-month t-now month)
+        (advance-day t-now day-of-month day-of-week)
+        (advance-hour t-now hour)
+        (advance-minute t-now minute)
         (t/floor t/minute))))
 
 (defn numeric?
